b0c22e54e0ac594d6aa8a9bc56a765a6904141cb
James N <email.james.noll@gmail.com>
2025-09-08 12:23:41 -0400

    Add code and debug toggle switch for dark/light theme button

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log("Portfolio website loaded!");
+
+//Add toggle for choosing light or dark theme
+const toggleButton = document.getElementById("theme-toggle");
+toggleButton.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});

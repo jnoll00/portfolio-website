3371a632bf42a1c567dd6a32c5c73cf4d819367b
James N <email.james.noll@gmail.com>
2025-09-08 13:11:53 -0400

    Modified dark-mode/theme toggle and style to ensure functionality and readbility in dark-mode

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ console.log("Portfolio website loaded!");
 //Add toggle for choosing light or dark theme
 const toggleButton = document.getElementById("theme-toggle");
 toggleButton.addEventListener("click", () => {
-  document.body.classList.toggle("dark");
+  document.body.classList.toggle("dark-mode"); //matches CSS class
 });
